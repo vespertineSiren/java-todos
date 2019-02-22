f0bad281c94d99009fd57a37dd4dc0198c00093b
Patrick Martin <patrickmartin88@gmail.com>
2019-02-22 08:23:19 -0800

    Added Config package - DbConfigutation, SQLiteDialect, WebConfiguration.

---
+++ src/main/java/com/lambdaschool/javatodo/Config/DBConfiguration.java
@@ -0,0 +1,19 @@
+package com.lambdaschool.javatodo.Config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DBConfiguration {
+
+    @Bean
+    public DataSource dataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.sqlite.JDBC");
+        dataSourceBuilder.url("jdbc:sqlite:todos.db");
+        return dataSourceBuilder.build();
+    }
+}
